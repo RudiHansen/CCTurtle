101299e2688226dd0f1f58c4cb11609490b38e15
Rudi Hansen <Rudi.Hansen@outlook.dk>
2023-05-16 10:29:36 +0200

    Add .gitignore updated README.md Added setup.lua

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 # CCTurtle
 Computercraft Turtle, used in minecraft for controlling a Turtle.
 
+# Information a turtle should know.
+Its Label
+Its Position
+Its Fuel level
+Its Inventory level
+
 # Plan
+Make setup script for turtle to set Turtle label, and get Turtle position and save position to .dat file
+
 
---
+++ setup.lua
@@ -0,0 +1,54 @@
+--[[
+    Setup script for ComputerCraft Turtles
+    Developed by Rudi Hansen, Start 2023-03-18
+
+    TODO
+
+    CHANGE LOG
+    2023-05-16 : Initial Version.
+]]
+local lable         = ""
+local homePos       = {x=0,y=0,z=0}
+local fuelLevel     = 0
+
+-- Setup Turtle Label
+lable = os.getComputerLabel()
+term.clear()
+term.setCursorPos(1,1)
+print("Label ="..lable)
+print("Enter new label, or enter to keep current label")
+local input = read()
+print(input)
+if input ~= "" then
+    os.setComputerLabel(input)
+end
+
+-- Setup Turtle Position
+term.clear()
+term.setCursorPos(1,1)
+print("Input Turtles x position")
+input = read()
+homePos.x = input
+print("Input Turtles y position")
+input = read()
+homePos.y = input
+print("Input Turtles z position")
+input = read()
+homePos.z= input
+print(homePos.x,homePos.y,homePos.z)
+
+-- Setup Turtle Fuellevel
+fuelLevel = turtle.getFuelLevel()
+
+-- Write location to file
+local locationFileName = "location.dat"
+local locationData = homePos.x .. "\n" .. homePos.y .. "\n" .. homePos.z .. "\n"
+
+local locationFile = fs.open(locationFileName, "w")
+if locationFile then
+    locationFile.write(locationData)
+    locationFile.flush()
+    locationFile.close()
+else
+    print "ERROR WRITING LOCATION DATA"
+end

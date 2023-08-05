6befb09f6cb5b5752b31c8d755b4e4fd709439d2
Rudi Hansen <Rudi.Hansen@outlook.dk>
2023-08-05 06:50:16 +0200

    Added moveHelper library

---
+++ lib/moveHelper.lua
@@ -0,0 +1,13 @@
+ --[[
+    moveHelper Function Library
+    Developed by Rudi Hansen, Start 2023-08-05
+
+    For helper functions used in the move library
+]]
+
+local moveHelper = {}
+
+function.moveHelper.nill()
+end
+
+return moveHelper
